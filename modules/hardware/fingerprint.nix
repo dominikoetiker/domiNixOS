388bed4f2b24fa320bfec71946411a303c3e083a
Dominik Oetiker <oetikerdominik@gmail.com>
2026-03-01 15:05:32 +0100

    fix: added user to input group when use fprint

--- modules/hardware/fingerprint.nix
+++ modules/hardware/fingerprint.nix
@@ -1,10 +1,15 @@
 {
   config,
   pkgs,
+  userConfig,
   ...
 }:
 
 {
+
+  # Add user to the docker group
+  users.users.${userConfig.username}.extraGroups = [ "input" ];
+
   # enable fingerprint reader
   services.fprintd.enable = true;
 }
