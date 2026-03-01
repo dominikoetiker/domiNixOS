e9039189b70dc9d27ea98262a1848ab6f23f4510
Dominik Oetiker <oetikerdominik@gmail.com>
2026-03-01 23:17:14 +0100

    fix: not use input user group anymore

--- modules/hardware/fingerprint.nix
+++ modules/hardware/fingerprint.nix
@@ -1,15 +1,10 @@
 {
   config,
   pkgs,
-  userConfig,
   ...
 }:
 
 {
-
-  # Add user to the docker group
-  users.users.${userConfig.username}.extraGroups = [ "input" ];
-
   # enable fingerprint reader
   services.fprintd.enable = true;
 }
