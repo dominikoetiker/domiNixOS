c4844d529c4373c72ffd22dc8e91929675f5f018
Dominik Oetiker <oetikerdominik@gmail.com>
2026-03-11 10:37:57 +0100

    feat: add catppuccin colors to fzf

--- modules/shell/shared.nix
+++ modules/shell/shared.nix
@@ -8,6 +8,7 @@
         VISUAL = "nvim";
         EDITOR = "nvim";
         MANPAGER = "sh -c 'col -bx | bat -l man -p'";
+        FZF_DEFAULT_OPTS = "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8";
         TEXMFHOME = "$HOME/.local/share/texmf";
         npm_config_prefix = "$HOME/.local";
       };
