999736b92c1f36b6ba79dab283b8cae526c5e2c6
Dominik Oetiker <oetikerdominik@gmail.com>
2026-03-18 23:38:14 +0100

    fix: removed not working birdtray tool

--- modules/communication/thunderbird.nix
+++ modules/communication/thunderbird.nix
@@ -1,24 +1,5 @@
-{ pkgs, userConfig, ... }:
+{ ... }:
 
 {
-  # activate Thunderbird
   programs.thunderbird.enable = true;
-
-  # install Birdtray for Thunderbird system tray icon
-  environment.systemPackages = [ pkgs.birdtray ];
-
-  # add autostart entry for Birdtray
-  home-manager.users.${userConfig.username} = {
-    xdg.configFile."autostart/birdtray.desktop".text = ''
-      [Desktop Entry]
-      Name=Birdtray
-      Comment=System tray icon for Thunderbird
-      Exec=${pkgs.birdtray}/bin/birdtray
-      Icon=birdtray
-      Terminal=false
-      Type=Application
-      Categories=Network;Email;
-      StartupNotify=false
-    '';
-  };
 }
